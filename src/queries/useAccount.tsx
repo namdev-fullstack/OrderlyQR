55f90d9e579ecd4ee3428d6ba90e82218be963ec
nguyennam1029 <nguyennam10320011@gmail.com>
2025-08-01 14:03:14 +0700

    handle change password

--- src/queries/useAccount.tsx
+++ src/queries/useAccount.tsx
@@ -13,3 +13,8 @@ export const useUpdateMeMutation = () => {
     mutationFn: accountApiRequest.updateMe,
   });
 };
+export const useChangePasswordMutation = () => {
+  return useMutation({
+    mutationFn: accountApiRequest.changePassword,
+  });
+};
